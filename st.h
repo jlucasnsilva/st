0483ce844b191505dd4777cf02354252f7661831
João Lucas <jlucas.nsilva@gmail.com>
2020-12-04 00:08:25 -0300

    Applied scrollback patches
    https://st.suckless.org/patches/scrollback/
    
    Patches:
    - st-scrollback-20200419-72e3f6c.diff
    - st-scrollback-mouse-20191024-a2c479c.diff
    - st-scrollback-mouse-altscreen-20200416-5703aa0.diff
    - st-scrollback-mouse-increment-0.8.2.diff

--- st.h
+++ st.h
@@ -83,6 +83,8 @@ void die(const char *, ...);
 void redraw(void);
 void draw(void);
 
+void kscrolldown(const Arg *);
+void kscrollup(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
@@ -90,6 +92,7 @@ void toggleprinter(const Arg *);
 void copyurl(const Arg *);
 
 int tattrset(int);
+int tisaltscr(void);
 void tnew(int, int);
 void tresize(int, int);
 void tsetdirtattr(int);
